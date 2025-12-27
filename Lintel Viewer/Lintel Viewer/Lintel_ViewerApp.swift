29edc46c9a000455603dd3634a5b1a0e476cae57
Zack Brown <zack@zackbrown.co.uk>
2025-12-27 14:54:32 +0000

    Implement basic meshing for perimeter

--- Lintel Viewer/Lintel Viewer/Lintel_ViewerApp.swift
+++ Lintel Viewer/Lintel Viewer/Lintel_ViewerApp.swift
@@ -7,7 +7,7 @@
 import SwiftUI
 
 @main
-struct Verdure_ViewerApp: App {
+struct Lintel_ViewerApp: App {
     
     var body: some Scene {
         
--- Sources/Lintel/Extensions/Mesh.swift
+++ Sources/Lintel/Extensions/Mesh.swift
@@ -18,6 +18,9 @@ extension Mesh {
         let tiles = footprint.tiles
         let perimeter = footprint.perimeter
         let vertices = footprint.vertices
+        let scale = Triangle.Scale.tile
+        let size = 0.2
+        let apex = Vector(0.0, scale.edgeLength, 0.0)
         
         var polygons: [Polygon] = []
         
@@ -29,12 +32,63 @@ extension Mesh {
                               corners)
             
             switch wedge {
+                        
+            case .corner(_,
+                         let corner):
                 
-            case .corner(let corner):
+                let corners = corner.edges.flatMap {
+                    
+                    $0.corners.filter { $0 != corner }
+                }
                 
+                guard let lhs = corners.first,
+                      let rhs = corners.last else { break }
                 
+                let v0 = tile.vertex(lhs).position(scale)
+                let v1 = tile.vertex(rhs).position(scale)
+                let v2 = tile.vertex(corner).position(scale)
+                let v3 = v2.lerp(v0, size)
+                let v4 = v2.lerp(v1, size)
+                let v5 = v3 + apex
+                let v6 = v4 + apex
                 
-            case .edge(let edge):
+                let p0 = [v4 + apex, v3 + apex, v2 + apex].path(.red)
+                let p1 = [v5, v6, v4, v3].path(.blue)
+                
+                guard let peak = Polygon(shape: p0),
+                      let edge = Polygon(shape: p1) else { break }
+                
+                polygons.append(contentsOf: [peak,
+                                             edge])
+                
+            case .edge(_,
+                       let edge):
+                
+                let corners = tile.corners.filter {
+                    
+                    !edge.corners.contains($0)
+                }
+                
+                guard let corner = corners.first,
+                      let lhs = edge.corners.first,
+                      let rhs = edge.corners.last  else { break }
+                
+                let v0 = tile.vertex(lhs).position(scale)
+                let v1 = tile.vertex(rhs).position(scale)
+                let v2 = tile.vertex(corner).position(scale)
+                let v3 = v0.lerp(v2, size)
+                let v4 = v1.lerp(v2, size)
+                let v5 = v3 + apex
+                let v6 = v4 + apex
+                
+                let p0 = [v1 + apex, v0 + apex, v3 + apex, v4 + apex].path(.green)
+                let p1 = [v3, v4, v6, v5].path(.blue)
+                
+                guard let peak = Polygon(shape: p0),
+                      let edge = Polygon(shape: p1) else { break }
+                
+                polygons.append(contentsOf: [peak,
+                                             edge])
                 
             case .tile: break
             }
